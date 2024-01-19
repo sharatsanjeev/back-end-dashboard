28afc57833fc66944c682c17c4cc8ee062af43f9
sharat sanjeev <sanjeevsharat@gmail.com>
2024-01-19 12:55:22 +0530

    9th video

---
+++ db/User.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
+const userschema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String
+})
+module.exports=mongoose.model("users",userschema);
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const User=require('./db/User');
+require("./db/config"); 
+const app=express();
+app.use(express.json())
+
+app.post('/register',async(req,res)=>{
+    let user=new User(req.body);
+    let result=await user.save();
+    res.send(result)
+ 
+})
+app.listen(4500);

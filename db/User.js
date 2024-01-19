6fa426eb1844efdc4ba8fd20c27a165bad1d2d85
sharat sanjeev <sanjeevsharat@gmail.com>
2024-01-19 18:51:12 +0530

    url changes

--- db/User.js
+++ db/User.js
@@ -1,9 +1,18 @@
 const mongoose=require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
 const userschema=new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
+    name:{
+        type: String,
+        required:true
+    },
+    email:{
+        type: String,
+        required:true
+    },
+    password:{
+        type: String,
+        required:true
+    }
 })
 module.exports=mongoose.model("users",userschema);
 

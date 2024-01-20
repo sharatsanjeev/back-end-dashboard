6bd18d513163f593b02f2cbdc49f987758725ad6
sharat sanjeev <sanjeevsharat@gmail.com>
2024-01-20 19:58:49 +0530

    login api created

--- index.js
+++ index.js
@@ -5,10 +5,34 @@ const app=express();
 const cors=require('cors')
 app.use(express.json())
 app.use(cors());
+
+
 app.post('/register',async(req,res)=>{
     let user=new User(req.body);
     let result=await user.save();
+    console.log(result)
+    result=result.toObject();
+    delete result.password
     res.send(result)
  
+})
+
+app.post("/login",async(req,resp)=>{
+    if(req.body.email && req.body.password){
+
+        let user=await User.findOne(req.body).select("-password");
+        if(user)
+        {
+            resp.send(user)
+        }else{
+            resp.send({result:"no user found"})
+        }
+
+    }
+    else
+    {
+        resp.send({result:" both name and password needed"})
+    }
+    
 })
 app.listen(5000);

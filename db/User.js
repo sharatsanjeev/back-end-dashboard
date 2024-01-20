edc2f34de159094d24c5d8fb9d1a311e95bbd972
sharat sanjeev <sanjeevsharat@gmail.com>
2024-01-20 09:18:30 +0530

    user changes to normal

--- db/User.js
+++ db/User.js
@@ -3,15 +3,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/e-comm');
 const userschema=new mongoose.Schema({
     name:{
         type: String,
-        required:true
+       
     },
     email:{
         type: String,
-        required:true
+   
     },
     password:{
         type: String,
-        required:true
+   
     }
 })
 module.exports=mongoose.model("users",userschema);
--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express=require('express');
 const User=require('./db/User');
 require("./db/config"); 
 const app=express();
+const cors=require('cors')
 app.use(express.json())
-
+app.use(cors());
 app.post('/register',async(req,res)=>{
     let user=new User(req.body);
     let result=await user.save();
